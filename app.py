6646631a819f42b1951cb2ec8bfaed431c477af0
crisverissimoo <167791829+crisverissimoo@users.noreply.github.com>
2025-07-12 12:37:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -45,9 +45,9 @@ if autorizada:
     with st.expander(txt("🧾 Ficha de Anamnese Clínica", "🧾 Ficha Clínica")):
         col_esq, col_centro, col_dir = st.columns([1, 2, 1])
         with col_centro:
-    st.markdown("<h4 style='text-align:center;'>🧾 Ficha de Anamnese Clínica</h4>", unsafe_allow_html=True)
+            st.markdown("<h4 style='text-align:center;'>🧾 Ficha de Anamnese Clínica</h4>", unsafe_allow_html=True)
 
-    with st.form("form_clinica"):
+            with st.form("form_clinica"):
                 perguntas = {
                     "glaucoma": txt("Possui glaucoma ou outra condição ocular diagnosticada?", "¿Tiene glaucoma u otra condición ocular diagnosticada?"),
                     "infeccao": txt("Tem blefarite, terçol ou outras infecções oculares?", "¿Tiene blefaritis, orzuelos u otras infecciones oculares?"),
@@ -121,6 +121,7 @@ if autorizada:
                                     "\n".join(info_detectados))
                         st.success("✅ " + txt("Cliente apta para continuar — ficha validada com sucesso.", "Cliente apta para continuar — ficha validada correctamente."))
                         st.session_state.ficha_validada = True
+
 
                         
 if st.session_state.ficha_validada:
