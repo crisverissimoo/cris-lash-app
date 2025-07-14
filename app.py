567f9735f4d81c52c4457cd2993cc286ae9a60cc
crisverissimoo <167791829+crisverissimoo@users.noreply.github.com>
2025-07-15 00:34:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,8 @@ if autorizada:
     with col_centro:
         with st.form("form_clinica"):
             with st.expander(txt("🧾 Ficha de Anamnese Clínica", "🧾 Historial de salud"), expanded=True):
-
+                 st.markdown("<h4 style='text-align:center;'>🧾 Ficha de Anamnese Clínica</h4>", unsafe_allow_html=True)
+                
                 perguntas = {
                     "glaucoma": txt("Possui glaucoma ou outra condição ocular diagnosticada?", "¿Tiene glaucoma u otra condición ocular diagnosticada?"),
                     "infeccao": txt("Tem blefarite, terçol ou outras infecções oculares?", "¿Tiene blefaritis, orzuelos u otras infecciones oculares?"),
