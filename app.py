0a35171e77d8ddcda16a38556ce30c802cbb5868
crisverissimoo <167791829+crisverissimoo@users.noreply.github.com>
2025-07-17 13:49:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -565,11 +565,13 @@ if st.session_state.get("efeito_escolhido"):
                 st.markdown("<hr style='margin-top:30px; margin-bottom:30px;'>", unsafe_allow_html=True)
 
                 col_img, col_txt = st.columns([1.6, 1.4])
-                with col_img:
-                    st.markdown(f"""
-                        <div style='text-align:center;'>
-                            <img src="{tipo['img']}" alt="{nome}" style="width: 100%; border-radius: 8px;">
-                        </div>
+               with col_img:
+    st.markdown(f"""
+        <div style='text-align:center;'>
+            <img src="{tipo['img']}" alt="{nome}" style="width: 100%; border-radius: 8px;">
+        </div>
+    """, unsafe_allow_html=True)
+
                 
                 with col_txt:
                     st.markdown(f"<h5 style='text-align:center;'>{nome} — 💶 {tipo['valor']}</h5>", unsafe_allow_html=True)
