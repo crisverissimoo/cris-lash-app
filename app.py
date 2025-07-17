9e0f7e741836fae33ae893de6c3e6dce3d1339c5
crisverissimoo <167791829+crisverissimoo@users.noreply.github.com>
2025-07-17 12:40:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -528,8 +528,6 @@ if st.session_state.ficha_validada:
 
 # 🎯 Bloco 2 — Escolha do Tipo (liberado somente após escolher o efeito)
 
-# Só exibe o bloco se efeito_escolhido estiver definido
-
 if st.session_state.get("efeito_escolhido"):
     col_esq, col_centro, col_dir = st.columns([1, 2, 1])
     with col_centro:
@@ -560,15 +558,15 @@ if st.session_state.get("efeito_escolhido"):
             }
 
             for nome, tipo in tipos.items():
-                 st.markdown("<hr style='margin-top:30px; margin-bottom:30px;'>", unsafe_allow_html=True)
+                st.markdown("<hr style='margin-top:30px; margin-bottom:30px;'>", unsafe_allow_html=True)
 
                 col_img, col_txt = st.columns([1.6, 1.4])
                 with col_img:
                     st.markdown(f"""
-                         <div style='text-align:center;'>
-                <img src="{tipo['img']}" alt="{nome}" style="width: 100%; border-radius: 8px;">
-            </div>
-        """, unsafe_allow_html=True)
+                        <div style='text-align:center;'>
+                            <img src="{tipo['img']}" alt="{nome}" style="width: 100%; border-radius: 8px;">
+                        </div>
+                    """, unsafe_allow_html=True)
 
                 with col_txt:
                     st.markdown(f"<h5 style='text-align:center;'>{nome} — 💶 {tipo['valor']}</h5>", unsafe_allow_html=True)
@@ -680,4 +678,3 @@ if st.session_state.get("efeito_escolhido") and st.session_state.get("tipo_aplic
                         </div>
                         """
                         st.markdown(cuidados_html, unsafe_allow_html=True)
-
